e64e41c35462355e49e478f774dfd02097b3c15d
Declan W <148910875+DeclanW1@users.noreply.github.com>
2025-08-01 11:26:06 +0100

    Added name to CV content to ensure it is displaying correctly and there are no errors

---
+++ src/app/cv/CVContent.tsx
@@ -0,0 +1,9 @@
+export default function CVContent() {
+    return (
+      <div className="text-black">
+        <h1 className="text-4xl font-bold mb-6 text-center">Declan W</h1>
+
+      </div>
+    );
+  }
+  
